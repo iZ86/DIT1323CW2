57659abee49b45b1b5a3b7a1264704848f21ed89
skyfoojs <143149814+skyfoojs@users.noreply.github.com>
2024-01-16 17:22:00 +0800

    Add files via upload

---
+++ CW2.java
@@ -0,0 +1,58 @@
+/*
+ Isaac Yeow Ming 23049679
+ Foo Jia Seng 23055155
+ */
+import javax.swing.*;
+
+public class CW2{
+    public static void main(String[] args){
+        String currency;
+        currency = JOptionPane.showInputDialog(null, "Please Insert Your Currency (SGD/USD)");
+        double excRate = 0;
+        String symbol= "$";
+
+        
+
+        double choose; 
+        choose = Double.parseDouble(JOptionPane.showInputDialog(null, "Please Insert Your Exchange Amount (MYR)"));
+        
+        double adminFee=0;
+        if(choose <=2000.00){
+            
+            adminFee = choose * 0.0025;
+        } 
+        else if(choose<=5000.00){
+            adminFee = choose * 0.005;
+        }
+        else if(choose<=10000.00){
+            adminFee = choose * 0.0075;
+        }
+        else if(choose>10000.00){
+            adminFee = choose * 0.01;
+        }
+
+        double total=0;
+
+        if("SGD".equalsIgnoreCase(currency)){
+            symbol= "$";
+            total= choose *0.28;
+        }
+        else if("USD".equalsIgnoreCase(currency)){
+            total= choose *0.21;
+            symbol= "$";
+        }
+        else if("EUR".equalsIgnoreCase(currency)){
+            total= choose *0.20;
+            symbol= "€";
+        }
+        else if("JPY".equalsIgnoreCase(currency)){
+            total= choose *31.23;
+            symbol= "¥";
+        }
+       
+        JOptionPane.showMessageDialog(null, "Total Amount Exchanged is"+ symbol + total);
+        JOptionPane.showMessageDialog(null, "Your Admin Fee is RM"+ adminFee);
+        
+        
+    }
+}
