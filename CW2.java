58b99b62a45bfb398a46073a5a8fead142a06638
skyfoojs <skyfoojs@gmail.com>
2024-01-18 15:48:24 +0800

    New Coursework2, everything is done take a look, but adminFee seems like got some problem

--- CW2.java
+++ CW2.java
@@ -7,53 +7,52 @@ import javax.swing.*;
 
 public class CW2{
     public static void main(String[] args){
+        while (true) {
+
         String currency;
-        currency = JOptionPane.showInputDialog(null, "Please Insert Your Currency \n(SGD/USD/JPY/EUR)");
-        double excRate = 0;
-        String symbol= "$";
-
+        currency = JOptionPane.showInputDialog(null, "Please Insert Your Currency");
         
-
         double choose; 
-        choose = Double.parseDouble(JOptionPane.showInputDialog(null, "Please Insert Your Exchange Amount (MYR)"));
+        choose = Double.parseDouble(JOptionPane.showInputDialog(null, "Please Insert Your Exchange Amount"));
         
-        double adminFee=0;
-        if(choose <=2000.00){
-            
-            adminFee = choose * 0.0025;
-        } 
-        else if(choose<=5000.00){
-            adminFee = choose * 0.005;
-        }
-        else if(choose<=10000.00){
-            adminFee = choose * 0.0075;
-        }
-        else if(choose>10000.00){
-            adminFee = choose * 0.01;
-        }
-
-        double total=0;
+        double currencyPrice=0;
 
         if("SGD".equalsIgnoreCase(currency)){
-            symbol= "$";
-            total= choose *0.28;
+            currencyPrice= choose *3.5098;
         }
         else if("USD".equalsIgnoreCase(currency)){
-            total= choose *0.21;
-            symbol= "$";
+            currencyPrice= choose *4.7156;
         }
         else if("EUR".equalsIgnoreCase(currency)){
-            total= choose *0.20;
-            symbol= "€";
+            currencyPrice= choose *5.1366;
         }
         else if("JPY".equalsIgnoreCase(currency)){
-            total= choose *31.23;
-            symbol= "¥";
+            currencyPrice= choose *0.0319;
         }
-       
-        JOptionPane.showMessageDialog(null, "Total Amount Exchanged is"+ symbol + total);
-        JOptionPane.showMessageDialog(null, "Your Admin Fee is RM"+ adminFee);
+
+        double adminFee=0;
+        if(currencyPrice <=2000.00){   
+            adminFee = currencyPrice * 0.0025;
+        } 
+        else if(currencyPrice<=5000.00){
+            adminFee = currencyPrice * 0.005;
+        }
+        else if(currencyPrice<=10000.00){
+            adminFee = currencyPrice * 0.0075;
+        }
+        else if(currencyPrice>10000.00){
+            adminFee = currencyPrice * 0.01;
+        }
+
+        double afterAdminPrice;
+        afterAdminPrice = currencyPrice - adminFee;
         
+       
+        
+        JOptionPane.showMessageDialog(null, "Total Amount Exchanged is RM"+ afterAdminPrice);
+        JOptionPane.showMessageDialog(null, "Admin Fee has been deduted\nYour Admin Fee is RM"+ adminFee);
+    }
+
         
     }
 }
