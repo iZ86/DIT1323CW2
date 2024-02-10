0803968654bcacf605c72465ab53e335ae30e559
iZLikesCS@gmail.com <iZLikesCS@gmail.com>
2024-02-11 00:12:52 +0800

    In CW2QN2 class, added getStudentIDFromUserInput, getStudentNameFromUserInput, getStudyLevelFromUserInput, validateStudentID, validateStudentName, validateStudyLevel, printInvalidUserInput, isStringContainAlphabetOnly and isStringContainSpecialCharacters method

--- QN2/CW2QN2.java
+++ QN2/CW2QN2.java
@@ -5,104 +5,26 @@ public class CW2QN2 {
 
     public static void main(String[] args) {
 
-        String studentID = "";
-        String studentName = "";
-        String studyLevel = "";
+        String studentID, studentName, studyLevel;
+        double[] scoresOfSubject = new double[6];
+        int numberOfSubjects = scoresOfSubject.length;
         Scanner s = new Scanner(System.in);
 
-        // Validates studentID input of user.
-        while (studentID.isBlank()) {
-            System.out.println("Please Input Your Student ID");
-            studentID = s.nextLine();
+        studentID = getStudentIDFromUserInput(s);
+        studentName = getStudentNameFromUserInput(s);
+        studyLevel = getStudyLevelFromUserInput(s);
 
-            // Validates every character of studentID.
-            for (int i = 0; i < studentID.length(); i++) {
-
-                boolean isValidStudentIDCharacter = false;
-
-                // Set isValidStudentIDCharacter to true, if the ith character of studentID is a number or an alphabet.
-                if (isCharacterAlphabet(studentID.charAt(i)) || isCharacterNumber(studentID.charAt(i))) {
-
-                    isValidStudentIDCharacter = true;
-                }
-
-                // If not valid character set to false, and prompt user for input again.
-                if (!(isValidStudentIDCharacter)) {
-                    System.out.println("Invalid character.");
-                    studentID = "";
-                    break;
-                }
-            }
-        }
-
-        // Validates studentName input of user.
-        while (studentName.isBlank()) {
-            System.out.println("Please Input Your Name");
-            studentName = s.nextLine();
-
-            // Validates every character of studentName.
-            for (int i = 0; i < studentName.length(); i++) {
-
-                boolean isValidStudentNameCharacter = false;
-
-                // Set isValidStudentNameCharacter to true, if the ith character of studentName is an alphabet.
-                if (isCharacterAlphabet(studentName.charAt(i))) {
-
-                    isValidStudentNameCharacter = true;
-                }
-
-                // If not valid character set to false, and prompt user for input again.
-                if (!(isValidStudentNameCharacter)) {
-                    System.out.println("Invalid character.");
-                    studentName = "";
-                    break;
-                }
-
-            }
-        }
-
-        // Validates studyLevel input of user.
-        while (studyLevel.isBlank()) {
-            System.out.println("Please Input Your Study Level");
-            studyLevel = s.nextLine();
-
-            // Validates every character of studyLevel.
-            for (int i = 0; i < studyLevel.length(); i++) {
-
-                boolean isValidStudyLevelCharacter = false;
-
-                // Set isValidStudyLevelCharacter to true, if the ith character of studyLevel is a number or an alphabet.
-                if (isCharacterAlphabet(studyLevel.charAt(i)) || isCharacterNumber(studyLevel.charAt(i))) {
-
-                    isValidStudyLevelCharacter = true;
-                }
-
-                // If not valid character set to false, and prompt user for input again.
-                if (!(isValidStudyLevelCharacter)) {
-                    System.out.println("Invalid character.");
-                    studyLevel = "";
-                    break;
-                }
-
-            }
-        }
-
-
-        String level = s.nextLine();
-
-        double[] storeScores = new double[5];
-        int size = storeScores.length;
 
         boolean j = true;
         while (j) {
             j = false;
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < numberOfSubjects; i++) {
                 System.out.println("Please Input Scores for Subject " + (i + 1) + ":");
 
-                storeScores[i] = s.nextDouble();
+                scoresOfSubject[i] = s.nextDouble();
 
-                for (double scores : storeScores) {
-                    if (storeScores[i] <= 0 || storeScores[i] > 100 || String.valueOf(scores).matches(".*[A-Za-z].*")) {
+                for (double scores : scoresOfSubject) {
+                    if (scoresOfSubject[i] <= 0 || scoresOfSubject[i] > 100 || String.valueOf(scores).matches(".*[A-Za-z].*")) {
                         j = true;
 
                     }
@@ -112,10 +34,126 @@ public class CW2QN2 {
 
         s.close();
 
-        Exam e = new Exam(studentID, studentName, level, storeScores);
+        Exam e = new Exam(studentID, studentName, studyLevel, scoresOfSubject);
 
         System.out.println(e);
 
+    }
+
+    /** Return studentID from user input after validation.
+     * If invalid, prompt user for new studentID input.
+     */
+    public static String getStudentIDFromUserInput(Scanner s) {
+
+        String studentID = "";
+
+        // Validates studentID input of user.
+        while (studentID.isBlank()) {
+            System.out.println("Please Input Your Student ID");
+            studentID = validateStudentID(s.nextLine());
+        }
+
+        return studentID;
+    }
+
+    /** Return studentName from user input after validation.
+     * If invalid, prompt user for new studentName input.
+     */
+    public static String getStudentNameFromUserInput(Scanner s) {
+
+        String studentName = "";
+
+        // Validates studentName input of user.
+        while (studentName.isBlank()) {
+            System.out.println("Please input your name");
+            studentName = validateStudentName(s.nextLine());
+        }
+
+        return studentName;
+    }
+
+    /** Return studyLevel from user input after validation.
+     * If invalid, prompt user for new studyLevel input.
+     */
+    public static String getStudyLevelFromUserInput(Scanner s) {
+
+        String studyLevel = "";
+
+        // Validates studyLevel input of user.
+        while (studyLevel.isBlank()) {
+            System.out.println("Please enter your study level.");
+            studyLevel = validateStudyLevel(s.nextLine());
+
+        }
+
+        return studyLevel;
+    }
+
+    /** Return the given studentID if valid.
+     * Otherwise, return empty string.
+     */
+    public static String validateStudentID(String studentID) {
+        if (isStringContainSpecialCharacters(studentID)) {
+            studentID = "";
+            printInvalidUserInput();
+        }
+        return studentID;
+    }
+
+    /** Return the given studentName if valid.
+     * Otherwise, return empty string.
+     */
+    public static String validateStudentName(String studentName) {
+        if (!(isStringContainAlphabetOnly(studentName))) {
+            studentName = "";
+            printInvalidUserInput();
+        }
+        return studentName;
+    }
+
+    /** Return the given studyLevel if valid.
+     * Otherwise, return empty string.
+     */
+    public static String validateStudyLevel(String studyLevel) {
+        if (isStringContainSpecialCharacters(studyLevel)) {
+            studyLevel = "";
+            printInvalidUserInput();
+        }
+        return studyLevel;
+    }
+
+    /** Prints a message for an invalid user input. */
+    public static void printInvalidUserInput() {
+        System.out.println("Invalid input.");
+    }
+
+
+    /** Return true iff every character in String STRING is an alphabet.
+     * Otherwise, return false.
+     */
+    public static boolean isStringContainAlphabetOnly(String string) {
+
+        for (int i = 0; i < string.length(); i++) {
+
+            if (!(isCharacterAlphabet(string.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** Return true iff every character in String STRING is a special character.
+     *  Otherwise, return false.
+     */
+    public static boolean isStringContainSpecialCharacters(String string) {
+
+        for (int i = 0; i < string.length(); i++) {
+
+            if (!(isCharacterNumber(string.charAt(i)) || isCharacterAlphabet(string.charAt(i)))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /** Return true, iff char number is part of base 10 numbers.
