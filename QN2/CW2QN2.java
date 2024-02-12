e7524509cc026fe1280f7aae8fadc70fb953918f
iZLikesCS@gmail.com <iZLikesCS@gmail.com>
2024-02-12 17:42:46 +0800

    Fixed whitespace and score > 100 and score < 100 validation, added new isValid methods, added new isCharacterDecimalPoint and isCharacterWhiteSpace method

--- QN2/CW2QN2.java
+++ QN2/CW2QN2.java
@@ -5,19 +5,19 @@ public class CW2QN2 {
 
     public static void main(String[] args) {
 
-        String studentID, studentName, studyLevel;
+        String studentID, studentName, studentLevel;
         int numberOfSubjects = 6;
         double[] scoresOfSubject;
         Scanner s = new Scanner(System.in);
 
         studentID = getStudentIDFromUser(s);
         studentName = getStudentNameFromUser(s);
-        studyLevel = getStudyLevelFromUser(s);
+        studentLevel = getStudentLevelFromUser(s);
         scoresOfSubject = getSubjectScoresFromUser(s, numberOfSubjects);
 
         s.close();
 
-        Exam e = new Exam(studentID, studentName, studyLevel, scoresOfSubject);
+        Exam e = new Exam(studentID, studentName, studentLevel, scoresOfSubject);
 
         System.out.println(e);
 
@@ -55,21 +55,21 @@ public class CW2QN2 {
         return studentName;
     }
 
-    /** Return studyLevel from user input after validation.
-     * If invalid, prompt user for new studyLevel input.
+    /** Return studentLevel from user input after validation.
+     * If invalid, prompt user for new studentLevel input.
      */
-    public static String getStudyLevelFromUser(Scanner s) {
+    public static String getStudentLevelFromUser(Scanner s) {
 
-        String studyLevel = "";
+        String studentLevel = "";
 
-        // Validates studyLevel input of user.
-        while (studyLevel.isBlank()) {
+        // Validates studentLevel input of user.
+        while (studentLevel.isBlank()) {
             System.out.println("Please enter your study level.");
-            studyLevel = validateStudyLevel(s.nextLine());
+            studentLevel = validateStudentLevel(s.nextLine());
 
         }
 
-        return studyLevel;
+        return studentLevel;
     }
 
     /** Return the scores for NUMBEROFSUBJECTS subjects from user input if valid.
@@ -100,7 +100,7 @@ public class CW2QN2 {
     public static String validateStudentID(String studentID) {
 
         // studentID must not contain any special characters.
-        return validateUserInput(studentID, !(isStringContainSpecialCharacters(studentID)));
+        return validateUserInput(studentID, isValidStudentID(studentID));
     }
 
     /** Return the given studentName if valid.
@@ -109,16 +109,16 @@ public class CW2QN2 {
     public static String validateStudentName(String studentName) {
 
         // studentName must not contain any special characters or numbers.
-        return validateUserInput(studentName, isStringContainAlphabetOnly(studentName));
+        return validateUserInput(studentName, isValidStudentName(studentName));
     }
 
-    /** Return the given studyLevel if valid.
+    /** Return the given studentLevel if valid.
      * Otherwise, return empty string.
      */
-    public static String validateStudyLevel(String studyLevel) {
+    public static String validateStudentLevel(String studentLevel) {
 
-        // studyLevel must not contain any special characters.
-        return validateUserInput(studyLevel, !(isStringContainSpecialCharacters(studyLevel)));
+        // studentLevel must not contain any special characters.
+        return validateUserInput(studentLevel, isValidStudentLevel(studentLevel));
     }
 
     /** Return the given score if valid.
@@ -146,89 +146,143 @@ public class CW2QN2 {
         System.out.println("Invalid input.");
     }
 
+    /** Return true iff String studentID does not contain any special characters.
+     * Otherwise, return false.
+     */
+    public static boolean isValidStudentID(String studentID) {
+
+        for (int i = 0; i < studentID.length(); i++) {
+            char character = studentID.charAt(i);
+
+            if (!(isCharacterAlphabet(character)) && !(isCharacterNumber(character))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** Return true iff String studentName does not contain any special characters excluding whitespace,
+     * and does not contain numbers,
+     * and there is no whitespace before first name.
+     * and there is no whitespace after last name.
+     * and there is no whitespace before or after another whitespace.
+     * Otherwise, return false.
+     */
+    public static boolean isValidStudentName(String studentName) {
+
+        for (int i = 0; i < studentName.length(); i++) {
+            char character = studentName.charAt(i);
+
+            // Return false if the character is a number or special character, excluding whitespace.
+            if (!(isCharacterAlphabet(character)) && !(isCharacterWhiteSpace(character))) {
+                return false;
+            }
+
+            if (isCharacterWhiteSpace(character)) {
+                // Return false, if there is two white spaces next to each other.
+                // Or, if there is white space in the beginning of the name.
+                // Or, if there is white space after last name.
+                if ((((i + 1) < studentName.length()) && (isCharacterWhiteSpace(studentName.charAt(i + 1))))
+                        || (i == 0)
+                        || (i == (studentName.length() - 1))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /** Return true iff String studentLevel does not contain any special characters,
+     * and there is no whitespace before first name.
+     * and there is no whitespace after last name.
+     * and there is no whitespace before or after another whitespace.
+     * Otherwise, return false.
+     */
+    public static boolean isValidStudentLevel(String studentLevel) {
+        for (int i = 0; i < studentLevel.length(); i++) {
+            char character = studentLevel.charAt(i);
+
+            // Return false if the character is a special character, excluding whitespace.
+            if (!(isCharacterAlphabet(character)) && !(isCharacterNumber(character)) && !(isCharacterWhiteSpace(character))) {
+                return false;
+            }
+
+            if (isCharacterWhiteSpace(character)) {
+                // Return false, if there is two white spaces next to each other.
+                // Or, if there is white space in the beginning of the name.
+                // Or, if there is white space after last name.
+                if ((((i + 1) < studentLevel.length()) && (isCharacterWhiteSpace(studentLevel.charAt(i + 1))))
+                        || (i == 0)
+                        || (i == (studentLevel.length() - 1))) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     /** Return true iff String SCORE is a valid score.
      * Otherwise, return false.
      */
     public static boolean isValidScore(String score) {
-        int numberOfDecimalPlaces = 0;
+        int numberOfDecimalPoint = 0;
 
         for (int i = 0; i < score.length(); i++) {
             char character = score.charAt(i);
 
             if (isCharacterDecimalPoint(character)) {
-                numberOfDecimalPlaces += 1;
+                numberOfDecimalPoint += 1;
             }
 
-            /* Return false, if character is not a number and not a decimal place,
-            or if it has more than one decimal places,
-            or if the whole score is just one decimal place,
-            or if it has more than 2 decimal place.
+            /* Return false, if character is not a number or a decimal place,
+            or if it has more than one decimal point,
+            or if the whole score is just one decimal point,
+            or if it has more than 2 decimal places.
              */
             if ((!isCharacterNumber(character) && !isCharacterDecimalPoint(character))
-                    || numberOfDecimalPlaces > 1
-                    || (numberOfDecimalPlaces == 1 && (score.length() == 1))
-                    || (numberOfDecimalPlaces == 1 && ((score.length() - i) > 3))) {
+                    || numberOfDecimalPoint > 1
+                    || (numberOfDecimalPoint == 1 && (score.length() == 1))
+                    || (numberOfDecimalPoint == 1 && ((score.length() - i) > 3))) {
                 return false;
             }
         }
-        
+
+        // subjectScore cannot be less than 0, or more than 100.
+        double subjectScore = Double.parseDouble(score);
+        if (subjectScore < 0 || subjectScore > 100) {
+            return false;
+        }
+
         return true;
     }
 
 
-    /** Return true iff every character in String STRING is an alphabet.
-     * Otherwise, return false.
-     */
-    public static boolean isStringContainAlphabetOnly(String string) {
-
-        for (int i = 0; i < string.length(); i++) {
-
-            if (!(isCharacterAlphabet(string.charAt(i)))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /** Return true iff every character in String STRING is a special character.
-     *  Otherwise, return false.
-     */
-    public static boolean isStringContainSpecialCharacters(String string) {
-
-        for (int i = 0; i < string.length(); i++) {
-
-            if (!(isCharacterNumber(string.charAt(i)) || isCharacterAlphabet(string.charAt(i)))) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     /** Return true, iff char CHARACTER is a decimal point.
      * Otherwise, return false.
      */
     public static boolean isCharacterDecimalPoint(char character) {
-        return character == 46;
+        return (character == 46);
+    }
+
+    /** Return true, iff char CHARACTER is a whitespace.
+     * Otherwise, return false.
+     */
+    public static boolean isCharacterWhiteSpace(char character) {
+        return (character == 32);
     }
 
     /** Return true, iff char NUMBER iS a number.
      * Otherwise, return false.
      */
     public static boolean isCharacterNumber(char number) {
-        if (number >= 48 && number <= 57) {
-            return true;
-        }
-        return false;
+        return (number >= 48 && number <= 57);
     }
 
     /** Return true, iff char CHARACTER is an alphabet, regardless of case.
      * Otherwise, return false.
      */
     public static boolean isCharacterAlphabet(char character) {
-        if ((character >= 65 && character <= 90)
-                || (character >= 97 && character <= 122)) {
-            return true;
-        }
-        return false;
+        return ((character >= 65 && character <= 90) || (character >= 97 && character <= 122));
     }
 }
