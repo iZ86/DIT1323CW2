cae8fd8f34c6cb5b21380535c546e26511acfb0b
iZLikesCS@gmail.com <iZLikesCS@gmail.com>
2024-02-12 10:09:53 +0800

    In Exam class, removed scores parameter from constructor and initialized it in constructor, added comments

--- QN2/Exam.java
+++ QN2/Exam.java
@@ -4,65 +4,82 @@ import java.text.DecimalFormat;
 // TODO: Maybe change the toString method.
 // TODO: Maybe revaluate the scores variable and methods.
 public class Exam {
+    /** Student ID. */
     private String studentID;
+    /** Student Name. */
     private String studentName;
-    private String studentLevel;  
+    /** Student level. */
+    private String studentLevel;
+    /** Subject scores of student. */
     private double[] scores;
 
-    public Exam(String studentID, String studentName, String studentLevel, double[] scores) {
+    /** A new exam object with 6 subject scores. */
+    public Exam(String studentID, String studentName, String studentLevel) {
         this.studentID = studentID;
         this.studentName = studentName;
         this.studentLevel = studentLevel;
-        this.scores = scores;
+        this.scores = new double[6];
 
     }
 
+    /** Set new String studentID. */
     public void setStudentID(String studentID) {
         this.studentID = studentID;
     }
 
+    /** Return String studentID. */
     public String getStudentID() {
         return studentID;
     }
 
+    /** Set new String studentName. */
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
 
+    /** Return String studentName. */
     public String getStudentName() {
         return studentName;
     }
 
+    /** Set new String studentLevel. */
     public void setStudentLevel(String studentLevel) {
         this.studentLevel = studentLevel;
     }
 
+    /** Return String studentLevel. */
     public String getStudentLevel() {
         return studentLevel;
     }
 
+    /** Set new double[] scores. */
     public void setScores(double[] scores) {
         this.scores = scores;
     }
 
+    /** Return double[] scores. */
     public double[] getScores() {
         return scores;
     }
+    
 
+    /** Return total score.*/
     public double calcTotalScores() {
         double totalScore = 0;
-        for(double score : scores) {
+        for (double score : scores) {
             totalScore += score;
         }
         return totalScore;
 
     }
 
+    /** Return average score. */
     public double calcAverageScores() {
      
         return calcTotalScores() / 6;
     }
 
+    /** Return max score. */
     public double getMax() {
         double maxScore = scores[0];
         for (double newScore : scores) {
@@ -70,7 +87,7 @@ public class Exam {
         }
         return maxScore;
     }
-    
+
     @Override
     public String toString() {
         DecimalFormat df = new DecimalFormat("##.###");
